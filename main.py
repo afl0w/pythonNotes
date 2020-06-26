78d438371fea1efed359138e4c048844ebb28a37
Alexis Flores <flrs.alexis@gmail.com>
2020-06-26 04:04:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#
+x = int(input('Please enter the first Integer: '))
+y = int(input('Please enter the second Integer: '))
+
+#user first name and last name input
+first_name = input('Enter your First Name: ')
+last_name = input('Enter your Last Name: ')
+
+#user output message display 
+print('The first integer is:',x)
+print('The second integer is:',y)
+print('Raised to the power of the second integer is:',x**y) 
+print('Multiplication Solution is:', x*y)
+print(first_name)
+print(last_name)
+print('Hello, ' + first_name + ' '+ last_name)
